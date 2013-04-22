3e3061435a62ba6f43ace4df4c4f670bb1c2a26f
Daniel Grech <danielgrech91@gmail.com>
2013-04-22 17:11:00 +1000

    Initial commit

---
+++ src/com/dgsd/android/graphing/TestActivity.java
@@ -0,0 +1,38 @@
+package com.dgsd.android.graphing;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public class TestActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LinearLayout ll = new LinearLayout(this);
+        ll.setPadding(50, 50, 50, 50);
+
+        final BarGraph graph = new BarGraph(this);
+        graph.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 500));
+        ll.addView(graph);
+
+        //For testing..
+        graph.add("Jan", 25, 0xFF558A06);
+        graph.add("Feb", 50, 0xFF558A06);
+        graph.add("Mar", 75, 0xFF558A06);
+        graph.add("Apr", 100, 0xFF558A06);
+
+        graph.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                graph.animateGraph();
+            }
+        }, 500);
+
+        setContentView(ll);
+    }
+}
